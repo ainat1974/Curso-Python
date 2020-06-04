de5dfba852760457eb9a128f2522c56a1c76d4be
Tânia Maria Rodrigues <taniagllbl@gmail.com>
2020-06-03 22:24:47 -0300

    Exercício 009
    Exercício 009 - Tabuada

---
+++ ex009.py
@@ -0,0 +1,16 @@
+# Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada
+# esta é a resposta do exercício resolvida por mim
+
+print('<<< Digite um número para que a sua tabuada seja mostrada abaixo >>>')
+n = int(input('<<< Número: >>> '))
+print('{} x  0 = {} \n'
+      '{} x  1 = {} \n'
+      '{} x  2 = {} \n'
+      '{} x  3 = {} \n'
+      '{} x  4 = {} \n'
+      '{} x  5 = {} \n'
+      '{} x  6 = {} \n'
+      '{} x  7 = {} \n'
+      '{} x  8 = {} \n'
+      '{} x  9 = {} \n'
+      '{} x 10 = {} \n'.format(n, n*0, n, n*1, n, n*2, n, n*3, n, n*4, n, n*5, n, n*6, n, n*7, n, n*8, 
