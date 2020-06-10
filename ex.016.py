c23eb509d818c788e9dbff81c51d5e222a444865
Tânia Maria Rodrigues <taniagllbl@gmail.com>
2020-06-10 20:08:35 -0300

    Exercício 016
    Exercício 016 - Quebrando um número

---
+++ ex.016.py
@@ -0,0 +1,5 @@
+# Crie um programa que leia um número Real qualquer pelo tecldo e mostre na tela a sua porção inteira.
+
+import math # desta forma importa todas as funcionalidades da bliblioteca math
+valor = float(input('Digite um valor: '))
+print('O valor digitado fou {} e a sua porção inteira é {}'.format(valor, math.trunc(valor)))
