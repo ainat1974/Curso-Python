f1ff015c3cb16fe82c6c4dbe9ad0d65064c2895f
Tânia Maria Rodrigues <taniagllbl@gmail.com>
2020-06-10 20:19:34 -0300

    Exercício 017
    Exercício 017 - Catetos e Hipotenusa

---
+++ ex017.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia o comprimento do cateto oposto
+# e do cateto adjacente de um triângulo, calcule e mostre
+# o comprimento da hipotenusa.
+
+# esta é a maneira matemática de resolução deste exercício
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co**2 + ca**2) ** (1/2)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
